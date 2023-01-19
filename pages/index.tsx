078c04d5c9ef15e665936d67b4cb2a9fa5bd3b12
rab554628 <107383259+rab554628@users.noreply.github.com>
2023-01-18 22:57:08 -0800

    FFEE22

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import { useState } from "react";
 import styles from "../styles/Theme.module.css";
 
 // Put Your NFT Drop Contract address from the dashboard here
-const myNftDropContractAddress = "0x003415D310D15826Ef25307034323D117dF36bED";
+const myNftDropContractAddress = "0x63AbbA64AfDf903D81438076007899e3973ee994";
 
 const Home: NextPage = () => {
   const { contract: nftDrop } = useContract(myNftDropContractAddress);
@@ -118,7 +118,7 @@ const Home: NextPage = () => {
                     }
                     // If the function fails, we can do something here.
                     onError={(error) => alert(error?.message)}
-                    accentColor="#FF00FF"
+                    accentColor="#FFEE22"
                     colorMode="dark"
                   >
                     {`Mint${quantity > 1 ? ` ${quantity}` : ""}${
