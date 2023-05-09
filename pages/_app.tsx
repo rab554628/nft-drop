29241e1e01efb3f687d2d2108057ab63a0656a4b
rab554628 <107383259+rab554628@users.noreply.github.com>
2023-05-09 09:35:23 +0530

    ww

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,21 +5,21 @@ import Head from "next/head";
 import ThirdwebGuideFooter from "../components/GitHubLink";
 
 // This is the chainId your dApp will work on.
-const activeChain = 1;
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
-        <Head>
-        <title>XTY World</title>
+      <Head>
+        <title>thirdweb NFT Drop Minting Customizable Page</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="XTY World's Minting dApp"
+          content="Learn How To Use Thirdweb's NFT Drop contract and create a customizable NFT Drop minting page"
         />
         <meta
           name="keywords"
-          content="NFT drop, nft collection"
+          content="Thirdweb, thirdweb NFT drop, how to make thirdweb nft drop, how to make nft collection thirdweb"
         />
       </Head>
       <Component {...pageProps} />
