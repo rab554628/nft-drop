88a52b345776fa76456a9350afee04b54564909d
rab554628 <107383259+rab554628@users.noreply.github.com>
2023-11-12 14:08:39 +0530

    ww

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ import styles from "../styles/Theme.module.css";
 import { parseIneligibility } from "../utils/parseIneligibility";
 
 // Put Your NFT Drop Contract address from the dashboard here
-const myNftDropContractAddress = "0x0Ef8613ba33F6C5D5418019cC80a525296c6B26B";
+const myNftDropContractAddress = "0xa6B13bD10BDd807F67D0Cfcd348192c972E165F5";
 
 const Home: NextPage = () => {
   const { contract: nftDrop } = useContract(myNftDropContractAddress);
